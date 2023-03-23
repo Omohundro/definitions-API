7a00d8db30758d80e8c36c694bc43dc49adc55c8
RS <omohundrro@hotmail.com>
2023-03-23 17:34:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<word>")
+def api(word):
+    definitions = df.loc[df["word"] == word]["definition"].squeeze()
+    result_dictionary = {"definition": definitions, "word": word}
+    return result_dictionary
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
